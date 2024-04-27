63e50d826c2502ddeaad81942ddd8c7eece8fd75
Ankesh kaneriya <149792800+Ankesh22nov@users.noreply.github.com>
2024-04-27 15:38:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var currentIndex = 0; // Current index of the active image
+    var images = [
+      "img/homeBackground.jpg",
+      "img/img8.jpg",
+    ];
+  
+    var heroSection = document.querySelector('.hero-section');
+    var prevButton = document.getElementById('prevButton');
+    var nextButton = document.getElementById('nextButton');
+  
+    // Function to update the background image
+    function updateBackground() {
+      heroSection.style.backgroundImage = "url('" + images[currentIndex] + "')";
+    }
+  
+    // Update background on page load
+    updateBackground();
+  
+    // Click event for previous button
+    prevButton.addEventListener('click', function() {
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+      updateBackground();
+    });
+  
+    // Click event for next button
+    nextButton.addEventListener('click', function() {
+      currentIndex = (currentIndex + 1) % images.length;
+      updateBackground();
+    });
+  });
+  
+
